7efa200878dd666da637af0c845645b12161e4ad
reyesenamorado97 <reyesenamorado@googlemail.com>
2022-06-30 07:56:59 -0400

    server is listening now

---
+++ lifetracker-api/server.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const cors = require("cors")
+const morgan = require("morgan");
+const { PORT } = require("../config")
+
+//const authRoutes = require("./routes/auth.js")
+
+const { BadRequestError, NotFoundError } = require("./utils/errors")
+
+const app = express()
+
+// enable cross-origin resource sharing
+app.use(cors());
+// parse incoming request with JSON
+app.use(express.json());
+// log request info
+app.use(morgan("tiny"));
+
+//app.use("/auth", authRoutes)
+
+
+app.use((req, res, next) => {
+    return next(new NotFoundError());
+});
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message;
+  
+    return res.status(status).json({
+      error: { message, status },
+    });
+});
+  
+
+
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running http://localhost:${PORT}`);
+  });
